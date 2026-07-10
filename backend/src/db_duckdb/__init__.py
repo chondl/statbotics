216e1d7b3831c2820316c1d232df88b6476f931c
Chris Hondl <chondl@gmail.com>
2026-07-10 09:22:13 -0700

    Add DuckDB read layer over Parquet blobs

---
+++ backend/src/db_duckdb/__init__.py
@@ -0,0 +1,29 @@
+from src.db_duckdb.main import (
+    get_event,
+    get_events,
+    get_match,
+    get_matches,
+    get_team,
+    get_team_event,
+    get_team_events,
+    get_team_year,
+    get_team_years,
+    get_teams,
+    get_year,
+    get_years,
+)
+
+__all__ = [
+    "get_event",
+    "get_events",
+    "get_match",
+    "get_matches",
+    "get_team_event",
+    "get_team_events",
+    "get_team_year",
+    "get_team_years",
+    "get_team",
+    "get_teams",
+    "get_year",
+    "get_years",
+]
